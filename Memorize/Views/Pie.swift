85d27e3cf0a6f09dd02b99bf073cd5f86daf700f
Amir Nourinia <amir.nouri.nia@gmail.com>
2023-06-28 17:07:20 +0200

    Create Pie.swift

---
+++ Memorize/Views/Pie.swift
@@ -0,0 +1,20 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Amir Nourinia on 27.06.23.
+//
+
+import SwiftUI
+
+struct Pie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Pie_Previews: PreviewProvider {
+    static var previews: some View {
+        Pie()
+    }
+}
